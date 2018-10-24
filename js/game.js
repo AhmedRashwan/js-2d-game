d7ecda8539206726ad403c88f3e2427e99056fe3
MahmoudWael <Mxheond9@gmail.com>
2018-10-25 00:05:44 +0200

    animate bouncing ball

---
+++ js/game.js
@@ -0,0 +1,31 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+var x = canvas.width/2;
+var y = canvas.height-30;
+var width = 50;
+var height = 50;
+var ballRadius = 10
+var dx = 2;
+var dy = -2;
+
+function animate() {
+    requestAnimationFrame(animate);  
+    
+    ctx.clearRect(0,0,canvas.width, canvas.height)
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#FF0000";
+    ctx.fill();
+    ctx.closePath();
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+    x += dx;
+    y += dy;
+}
+
+animate();
